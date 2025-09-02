8732cbe6402b67a65f0210004ce7dbcb0d97f13b
jeandejesus <jeanfreirecontato@outlook.com>
2025-09-02 09:23:31 -0300

    update health

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 
 @Controller('health')
 export class HealthController {
+  private readonly logger = new Logger(HealthController.name);
+
   @Get()
   check() {
+    this.logger.log('API funcionando normalmente!', new Date());
+
     return {
       status: 'ok',
       timestamp: new Date(),
