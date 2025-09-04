932e6c0d9f1e3403102a89645003750bbee73fb6
jeandejesus <jeanfreirecontato@outlook.com>
2025-09-04 20:38:08 -0300

    checking online system

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 
 @Controller('health')
 export class HealthController {
+  private readonly logger = new Logger(HealthController.name);
+
   @Get()
   check() {
-    return {
-      status: 'ok',
-      timestamp: new Date(),
-      message: 'API funcionando normalmente!',
-    };
+    try {
+      this.logger.log('API funcionando normalmente!', new Date());
+
+      return {
+        status: 'ok',
+        timestamp: new Date(),
+        message: 'API funcionando normalmente!',
+      };
+    } catch (error) {
+      this.logger.log('API funcionando normalmente!', error);
+    }
   }
 }
--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -56,6 +56,7 @@ export class NotificationsService {
 
     try {
       await webpush.sendNotification(subscriptionRecord.subscription, payload);
+      this.logger.log('notificações enviada');
     } catch (error: unknown) {
       console.error('Falha ao enviar notificação:', error);
 
